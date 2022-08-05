06db73c7a7ad2e7726bc5f65ffc978c2e4e29aee
MahfujAhsan <ahsanmahfuj@gmail.com>
2022-08-05 07:07:29 +0600

    wallet connected

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../../assets/oneobit.png';
 import './Navbar.css';
+import { ethers } from 'ethers';
 
 const Navbar = () => {
+    const [walletAddress, setWalletAddress] = useState("");
+    const requestAccount = async () => {
+        if (window.ethereum) {
+            try {
+                const accounts = await window.ethereum.request({
+                    method: "eth_requestAccounts",
+                });
+                setWalletAddress(accounts[0]);
+            } catch (error) {
+                console.log('Error connecting...');
+            }
+        } else {
+            alert('Meta Mask not detected');
+        }
+    }
+    
+    async function disconnectWallet() {
+        setWalletAddress()
+    }
     return (
         <nav className='lg:flex justify-between items-center px-3 lg:px-32'>
             <div className='w-6/12 mx-auto lg:mx-0 lg:w-2/12'>
@@ -18,7 +38,11 @@ const Navbar = () => {
                 </ul>
             </div>
             <div className='sign-btn-container w-4/12 mx-auto lg:mx-0 lg:w-auto'>
-                <button className='sign-btn font-semibold text-white'><Link to="#">Sign In</Link></button>
+                {!walletAddress ? <button onClick={requestAccount} className='sign-btn font-semibold text-white'>
+                    {!walletAddress ? <>Sign In</> : ""}
+                </button> : <button onClick={disconnectWallet} className='sign-btn font-semibold text-white'>
+                        <p>{walletAddress ? <>{walletAddress}</> : ""}</p>
+                    </button>}
             </div>
         </nav>
     );
